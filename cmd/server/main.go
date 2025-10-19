657daebd15daac77564e7e7a9b45d6a8c0e5c8df
Pavlenko Dmitry <Apofeoz@tut.by>
2025-10-19 17:36:36 +0300

    Add concurrent server and job scheduling using gocron
    Implemented a new cron job scheduler that runs concurrently with the HTTP server. The server starts on a separate goroutine, while a scheduled job prints a message every 10 seconds. This enhances the application's functionality by allowing background tasks to run alongside the main server process.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync"
+	"time"
 
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-co-op/gocron/v2"
 )
 
 const (
@@ -17,5 +21,56 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	http.ListenAndServe(httpPort, r)
+
+	// create a scheduler
+	s, err := gocron.NewScheduler()
+	if err != nil {
+		panic(err)
+	}
+
+	j, err := newCronJob(s)
+	if err != nil {
+		fmt.Printf("error: %v", err)
+	}
+
+	wg := sync.WaitGroup{}
+	wg.Add(2)
+
+	go func () {
+		defer wg.Done()
+
+		fmt.Println("starting server on port", httpPort)
+		err = http.ListenAndServe(httpPort, r)
+		if err != nil {
+			panic(err)
+		}
+	}()
+
+	go func () {
+		defer wg.Done()
+
+		fmt.Printf("starting job: %v\n", j.ID())
+		s.Start()
+	}()
+
+	wg.Wait()
+}
+
+func newCronJob(s gocron.Scheduler) (gocron.Job, error) {
+	// add a job to the scheduler
+	job, err := s.NewJob(
+		gocron.DurationJob(
+			10*time.Second,
+		),
+		gocron.NewTask(
+			func() {
+				fmt.Println("Hello!")
+			},
+		),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return job, nil
 }
